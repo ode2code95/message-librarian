0f84ec06fdde61b42cffa0f3de2a01cb53575727
CodeWhiz95 <sg.tla@emypeople.net>
2017-06-11 22:45:46 -0400

    Move database initialization and loading logic to main.cpp. Restructure logic to attempt corrections in the proper order, with detailed error output.

--- main.cpp
+++ main.cpp
@@ -1,8 +1,13 @@
 #include "mainwindow.h"
 #include <QApplication>
 #include <QSqlDatabase>
+#include <QSqlTableModel>
+#include <QSqlError>
+#include <QSqlQuery>
 
 bool InitDatabase();
+bool LoadDatabase();
+bool CreateNewDatabase();
 
 int main(int argc, char *argv[])
 {
@@ -11,6 +16,9 @@ int main(int argc, char *argv[])
     //Create connection to database, abort on error
     if (!InitDatabase())
         return 1;
+
+    if (!LoadDatabase())
+        return 2;
 
     MainWindow w;
     w.show();
@@ -28,9 +36,59 @@ bool InitDatabase()
     bool ok = db.open();
 
     if (!ok) {
-        QMessageBox::warning(0, "Error", "Cannot open database. Contact your support team for assistance.");
+        QMessageBox::warning(0, "Error", "Cannot open database. Please contact your support team for assistance.");
     }
 
     return ok;
+}
+
+bool LoadDatabase()
+{
+    QSqlTableModel model(0, QSqlDatabase::database());
+    model.setTable("sermon");
+    // Test code;
+    /*int row = 0;
+    model.insertRows(row, 1);
+    model.setData(model.index(row, 1), "The Fear of the Lord is the Beginning of Wisdom");
+    model.setData(model.index(row, 2), "Lloyd Mast");
+    model.setData(model.index(row, 3), "Mechanicsville Mennonite Church");
+    model.setData(model.index(row, 4), QDate::currentDate());
+    model.setData(model.index(row, 5), "4th Sunday Morning");
+    model.setData(model.index(row, 6), "Greetings in Jesus' name this morning . . .");*/
+    if (model.lastError().type() != QSqlError::NoError) {
+        int result = QMessageBox::warning(0, "Error", "Cannot load database. Error details: " + model.lastError().text() +
+                                          "\n Do you want to initialize a new one with default values?",
+                                          QMessageBox::Yes, QMessageBox::No, QMessageBox::NoButton);
+        if (result == QMessageBox::Yes) {
+            if (CreateNewDatabase()) {
+                QMessageBox::information(0, "Operation successful", "New database has been successfully created. Use the <b>New</b> button from the <b>File</b> menu to add entries.");
+                return true;
+            }
+            return false;
+        } else if (result == QMessageBox::No) {
+            return false;
+        }
+    }
+    // No error occurred. Proceed with program loading.
+    return true;
+}
+
+bool CreateNewDatabase()
+{
+    QSqlQuery query("CREATE TABLE sermon ("
+                     "id INTEGER PRIMARY KEY AUTOINCREMENT,"
+                     "title VARCHAR(40) NOT NULL,"
+                     "speaker VARCHAR(40) NOT NULL,"
+                     "location VARCHAR(40) NOT NULL,"
+                     "date VARCHAR(40) NOT NULL,"
+                     "description VARCHAR(40) NOT NULL,"
+                     "transcription VARCHAR(200) NOT NULL);", QSqlDatabase::database());
+
+    if (!query.isActive()) {
+        QMessageBox::warning(0, "Error", "Cannot create new database. Error details: " +
+                             query.lastError().text() + "\n Contact your support team for assistance.");
+        return false;
+    }
+    return true;
 }
 
--- mainwindow.cpp
+++ mainwindow.cpp
@@ -2,11 +2,7 @@
 #include "ui_mainwindow.h"
 #include "settingswindow.h"
 #include <QFileDialog>
-#include <QSqlTableModel>
-#include <QSqlError>
 #include <QDate>
-
-#include <QSqlQuery>
 
 MainWindow::MainWindow(QWidget *parent) :
     QMainWindow(parent),
@@ -39,33 +35,6 @@ void MainWindow::on_actionNew_triggered()
           tr("Please select a sermon recording . . ."), defaultOpenFrom, tr("Audio Files (*.wav *.mp3)"));
     if (fileNames.count() == 0) {
         return;
-    }
-
-    /*QSqlQuery query("CREATE TABLE sermon ("
-                     "id INTEGER PRIMARY KEY AUTOINCREMENT,"
-                     "title VARCHAR(40) NOT NULL,"
-                     "speaker VARCHAR(40) NOT NULL,"
-                     "location VARCHAR(40) NOT NULL,"
-                     "date VARCHAR(40) NOT NULL,"
-                     "description VARCHAR(40) NOT NULL,"
-                     "transcription VARCHAR(200) NOT NULL);", QSqlDatabase::database());
-
-    if (!query.isActive()) {
-        QMessageBox::warning(this, "Error", query.lastError().text());
-    }*/
-
-    QSqlTableModel model(this, QSqlDatabase::database());
-    model.setTable("sermon");
-    int row = 0;
-    model.insertRows(row, 1);
-    model.setData(model.index(row, 1), "The Fear of the Lord is the Beginning of Wisdom");
-    model.setData(model.index(row, 2), "Lloyd Mast");
-    model.setData(model.index(row, 3), "Mechanicsville Mennonite Church");
-    model.setData(model.index(row, 4), QDate::currentDate());
-    model.setData(model.index(row, 5), "4th Sunday Morning");
-    model.setData(model.index(row, 6), "Greetings in Jesus' name this morning . . .");
-    if (!model.submitAll()) {
-        QMessageBox::warning(this, "Error", model.lastError().text());
     }
 }
 
